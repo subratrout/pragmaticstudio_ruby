5b0e00d338a587cf092e5dc05816bd419b57a636
subratrout <subratnrout@gmail.com>
2016-08-26 18:27:34 -0600

    Added README and string manipulation

--- studio_game.rb
+++ studio_game.rb
@@ -3,7 +3,23 @@ name2 = "curly"
 name3 = "moe"
 
 health1 = 60
+health2 = 125
+health3 = 100
 
-puts "#{name1}'s health is #{health1}"
+puts "#{name1.capitalize}'s health is #{health1}."
+puts "#{name2.upcase}'s health is #{health2}."
+text = "#{name3.capitalize}'s health is #{health3}."
+
+puts text.center(50, "*")
+
 
 puts "Players: \n\t#{name1}\n\t #{name2} \n\t #{name3}"
+
+
+# health2 = health1
+
+# puts "#{name2.upcase}'s health is #{health2}"
+
+# health1 = 30
+# puts "#{name1.capitalize}'s health is #{health1}"
+# puts "#{name2.upcase}'s health is #{health2}"
