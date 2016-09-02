53aeb077f534b148f29a3d5d357540fa624bab17
subratrout <subratnrout@gmail.com>
2016-09-02 00:12:05 -0600

    Classes, attributes and array

--- studio_game.rb
+++ studio_game.rb
@@ -34,23 +34,34 @@
 # puts "#{name2.upcase}'s health is #{health2}"
 # 
 class Player
+  attr_reader :health
+  attr_accessor :name
+
   def initialize(name, health = 100)
     @name = name.capitalize
     @health = health
   end
 
   def to_s
-    "I am #{@name} with a health of #{@health}"
+    "I am #{@name} with a health of #{@health} and a score of #{score}"
   end
 
   def blam
     @health = @health - 10
-    "#{@name} got blammed!"
+    puts "#{@name} got blammed!"
   end
 
   def woot
     @health = @health + 15
-    "#{@name} got wooted!"
+    puts "#{@name} got wooted!"
+  end
+
+  def score
+    @health + @name.length
+  end
+
+  def name=(new_name)
+    @name = new_name.capitalize
   end
 end
 
@@ -67,4 +78,28 @@ player3.blam
 puts player3
 player3.woot
 puts player3
+
+puts player1.health
+puts player2.health
+puts player3.health
+
+player2.name = "laurence"
+
+puts player2.name
+
+players = [player1, player2, player3]
+
+puts "There are #{players.size} players in the game."
+
+players.each do |player|
+  player.blam
+  player.woot
+  player.woot
+  puts "I am #{player.name} with a health of #{player.health} and a score of #{player.score}."
+end
+
+players.pop
+player4 = Player.new("shemp", 90)
+
+players.push(player4)
 
